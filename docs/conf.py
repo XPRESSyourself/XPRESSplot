c4d730f7c2acb067002af37379decf853b088b85
Jordan Berg <j-berg@users.noreply.github.com>
2019-07-03 16:41:45 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Jordan A. Berg'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.5-beta'
+release = '0.0.6-beta'
 
 
 # -- General configuration ---------------------------------------------------
