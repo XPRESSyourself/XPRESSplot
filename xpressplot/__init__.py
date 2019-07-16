d71986311d9ad2a3bdc46a12d5fd65dea9d355f1
j-berg <j-berg@users.noreply.github.com>
2019-07-16 03:33:15 -0600

    Prep for versioning, push forward to be concurrent with xpresspipe

--- xpressplot/__init__.py
+++ xpressplot/__init__.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.0.8-beta'
+__version__ = '0.2.0-beta'
 
 from .fetch import *
 from .format import *
